fdd6680e18fac986e4843c4b820e8b29761fc4b5
Kundan450 <122659362+Kundan450@users.noreply.github.com>
2026-06-11 11:58:40 +0530

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-sheet_url = "https://azureinfogroup-my.sharepoint.com/:x:/r/personal/kundan_jadhav_data-axle_com/_layouts/15/Doc.aspx?sourcedoc=%7BD2891A1C-74D0-48D2-AF26-B8C3E4CC319E%7D&file=Order%20Tracker.xlsx&wdLOR=cEA699AB1-DEA9-4652-A4D4-62B48A3970FE&fromShare=true&action=default&mobileredirect=true"
-
-df = pd.read_csv(sheet_url)
